5d4291e16b1f86e0d03e9ad2f0595ec6b92cdb61
Yashvir Pal <yashveersoftwere06@gmail.com>
2025-07-15 03:04:16 +0530

    Add Recourse file

--- laravel-react-admin-panel/routes/web.php
+++ laravel-react-admin-panel/routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\UserController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 require __DIR__.'/backend.php';
 
-Route::get('/', [\App\Http\Controllers\Frontend\HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
